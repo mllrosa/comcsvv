18791a51c1e1d3cd1ce92f2292d065374551d715
mllrosa <mllr8sa@gmail.com>
2025-07-30 01:41:24 -0300

    Atualizar o main.py

--- comcsv/main.py
+++ comcsv/main.py
@@ -3,8 +3,21 @@ import csv
 with open("dados.csv",newline="",encoding="utf-8") as arquivo:
     arquivo_csv = csv.DictReader(arquivo)
 
-    #dados = list(arquivo_csv)
+    nome_procurado = "Damián Ramírez"
+
     for linha in arquivo_csv:
         if linha ["Author(s)"] == "Fortunato, Victoria Della Ventura, Hernández, Nahuel Nicolás, Vázquez, Damián Ramírez":
             linha["Author(s)"].split(',')
             print(linha["Author(s)"])
+
+
+
+
+
+
+    for linha in arquivo_csv:
+        autores = linha["Author(s)"].split(',')
+        autores = [a.strip() for a in autores]
+
+        if nome_procurado in autores:
+            print(nome_procurado)
